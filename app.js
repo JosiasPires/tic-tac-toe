b99aab8b1f926c73a222a361a60d6372bccfba4e
JosiasPires <akiles26@protonmail.com>
2023-09-16 19:26:15 -0300

    Create marker logic

--- app.js
+++ app.js
@@ -2,26 +2,38 @@ const game = (
     () => {
         console.log('hey')
         const reset = '--';
-        const start = () => {
+        const displayBoard = () => {
             for (let i = 0; i < 9; i++) {
                 gameBoard.cells[i].textContent = gameBoard.board[i];
             }
         };
-        const state = 0;
-        const mark = '--';
+        let state = 0;
+        const mark = function() {
+            if (this.textContent === '') {
+                if (state == 0) {
+                    player1.mark(this);
+                    state = 1;
+                }
+                else {
+                    player2.mark(this);
+                    state = 0;
+                }
+            }
+        };
+        
         const displayWinner = (winner) => {
             document.querySelector('winner').textContent = winner;
         };
-        return {reset, start, mark, displayWinner}
+        return {reset, displayBoard, mark, displayWinner};
     }
 )();
 
 const gameBoard = (
     () => {
-        const board = ['X', 'O', 'X', 'X', 'O', 'O', 'X', 'X', 'O'];
+        const board = ['', '', '', '', '', '', '', '', ''];
         const cells = Array.from(document.querySelectorAll('.cell'));
         for (let cell of cells) {
-            // cell.addEventListener('click', game.mark);
+            cell.addEventListener('click', game.mark);
         }
         return {board, cells}
     }
@@ -29,12 +41,14 @@ const gameBoard = (
 
 const Player = function(name, marker) {
     const mark = function(cell) {
-        if (cell.textContent == '') {
-            cell.textContent = marker;
-            gameBoard.board[Number(cell.getAttribute('data-position'))] = marker;
-        }
+        cell.textContent = marker;
+        gameBoard.board[Number(cell.getAttribute('data-position'))] = marker;
     }
     return {name, marker, mark};
 }
 
-game.start();
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
+const players = [player1, player2];
+
+game.displayBoard();
