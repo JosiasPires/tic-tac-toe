22cf5abdb71ef9b4b21e3c093b883d11850e0d29
JosiasPires <akiles26@protonmail.com>
2023-09-16 03:20:47 -0300

    Create basic gameboard grid

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const Player = function(name, marker) {
     const mark = function(cell) {
         if (cell.textContent == '') {
             cell.textContent = marker;
+            cell.setAttribute(cl)
             gameBoard.board[Number(cell.getAttribute('data-position'))] = marker;
         }
     }
     return {name, marker, mark};
 }
-
 
