f95fd3059c14fdc5ad1355c274ad56aebdbd48ea
JosiasPires <akiles26@protonmail.com>
2023-09-16 03:26:11 -0300

    Remove unnecessary line

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const Player = function(name, marker) {
     const mark = function(cell) {
         if (cell.textContent == '') {
             cell.textContent = marker;
-            cell.setAttribute(cl)
             gameBoard.board[Number(cell.getAttribute('data-position'))] = marker;
         }
     }
