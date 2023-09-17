efcd72dea323fdd4ca956dad944b44ac4ffc1225
JosiasPires <akiles26@protonmail.com>
2023-09-17 03:42:21 -0300

    I got carried away

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const game = (
     () => {
-        const congratBox = document.querySelector('.congratBox');
-        const winnerSpan = document.querySelector('.winner');
-        const congratMessage = document.querySelector('.congratMessage');
-        const reset = () => {
+        const reset = function() {
             for (let i = 0; i < gameBoard.board.length; i++) {
                 gameBoard.board[i] = '';
             }
             displayBoard();
+            state = 0;
+        };
+        let players, p1, p2;
+        const start = (player1, player2) => {
+            if (player1 === undefined && player2 === undefined) {
+                p1 = Player('Player 1', 'X');
+                p2 = Player('Player 2', 'O');
+            }
+            else if (player1 === undefined) {
+                p1 = Player('Player 1', 'X');
+                p2 = Player(player2, 'O');
+            }
+            else if (player2 === undefined) {
+                p1 = Player(player1, 'X');
+                p2 = Player('Player 2', 'O');
+            }
+            else {
+                p1 = Player(player1, 'X');
+                p2 = Player(player2, 'O');
+            }
+            players = [p1, p2];
         };
         const displayBoard = () => {
             for (let i = 0; i < 9; i++) {
@@ -15,21 +33,23 @@ const game = (
             }
         };
         let state = 0;
+        let winner;
+        let tie;
         const mark = function() {
             if (this.textContent === '') {
                 if (state == 0) {
-                    player1.mark(this);
+                    p1.mark(this);
                     state = 1;
                 }
                 else {
-                    player2.mark(this);
+                    p2.mark(this);
                     state = 0;
                 }
             }
             checkWinner();
         };
         const checkWinner = () => {
-            let winner = false;
+            winner = false;
             let board = gameBoard.board;
             for (let player of players) {
                 let marker = player.marker;
@@ -54,34 +74,85 @@ const game = (
                     winner = player;
                 }
             }
-            if (winner) {
-                winnerSpan.textContent = winner.name;
-                congratBox.showModal();
-            }
+            if (winner) displayResult(winner.name);
             else {
                 // Check tie
-                let tie = true;
+                tie = true;
                 for (let cell of board) {
                     if (cell == '') tie = false;
                 }
-                if (tie) {
-                    congratMessage.textContent = "It's a tie!";
-                    congratBox.showModal();
-                }
+                if (tie) displayResult();
             }
         }
-        const displayWinner = (winner) => {
-            document.querySelector('winner').textContent = winner;
+        const displayResult = (result) => {
+            if (result === undefined) {
+                dialog.congratMessage.textContent = "It's a tie!";
+                dialog.congratBox.showModal();
+            }
+            else {
+                dialog.congratMessage.textContent = `Congrats! The winner is ${result}`
+                dialog.congratBox.showModal();
+            }
         };
-        return {reset, displayBoard, mark, displayWinner, checkWinner};
+        return {reset, start, displayBoard, mark, checkWinner, players};
+    }
+)();
+
+const restart = (
+    () => {
+        const restartBtn = document.querySelector('.restart');
+        restartBtn.addEventListener('click', () => {
+            game.reset();
+            dialog.startBox.showModal();
+        })
+    }
+)();
+
+const dialog = (
+    () => {
+        const congratBox = document.querySelector('.congratBox');
+        const startBox = document.querySelector('.startBox');
+        const againBtn = document.querySelector('.againBtn')
+        const cancelStart = document.querySelector('#cancel');
+        const winnerSpan = document.querySelector('.winner');
+        const congratMessage = document.querySelector('.congratMessage');
+        const player1 = document.querySelector('#player1');
+        const player2 = document.querySelector('#player2');
+        const startBtn = document.querySelector('#start');
+        startBtn.addEventListener('click', e => {
+            e.preventDefault();
+            game.reset();
+            game.start(player1.value, player2.value);
+            console.log(player1.value, player2.value);
+            startBox.close();
+        });
+        cancelStart.addEventListener('click', e => {
+            e.preventDefault();
+            if (!(game.players)) game.start();
+            else {
+                game.start(player1.value, player2.value);
+                console.log(player1.value, player2.value);
+            }
+            startBox.close();
+        });
+        againBtn.addEventListener('click', () => {
+            startBox.showModal();
+        });
+        return {congratBox, startBox, winnerSpan, congratMessage};
     }
 )();
 
 const gameBoard = (
     () => {
         const board = ['', '', '', '', '', '', '', '', ''];
+        const grid = document.querySelector('.grid');
         const cells = Array.from(document.querySelectorAll('.cell'));
         for (let cell of cells) {
+            cell.addEventListener('click', () => {
+                if (game.winner || game.tie) {
+                    grid.setAttribute('class', 'grid disabled');
+                }
+            })
             cell.addEventListener('click', game.mark);
         }
         return {board, cells}
@@ -95,9 +166,5 @@ const Player = function(name, marker) {
     }
     return {name, marker, mark};
 }
-
-const player1 = Player('Player 1', 'X');
-const player2 = Player('Player 2', 'O');
-const players = [player1, player2];
 
 game.displayBoard();
